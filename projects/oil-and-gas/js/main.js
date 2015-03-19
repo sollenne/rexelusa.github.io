a4da441dfdde8da823f261033f68c414411f94a3
Rich Sollenne <Rich.Sollenne@Rexel.com>
2015-03-19 16:09:21 -0500

    oil/gas

--- projects/oil-and-gas/js/main.js
+++ projects/oil-and-gas/js/main.js
@@ -1,11 +1,21 @@
-	
-	$(window).load(function(){
-      $('.flexslider').flexslider({
-        animation: "slide",
-        start: function(slider){
-          $('body').removeClass('loading');
-        }
-      });
-    });
-    
-    
+$(document).ready(function() {
+	$('.tabs > div').click(function() {
+		var $currentTabIndex = $(this).index();
+		$('.content-inner').find('section').eq($currentTabIndex).stop().fadeIn().siblings().fadeOut();
+		$(this).addClass('active').siblings().removeClass('active');
+
+		var $currentTabPosition = $(this).position().left;
+		$('.tab-bg').stop().animate({'left': $currentTabPosition});
+	});
+
+	$('.tabs > div').mouseenter(function() {
+		var $currentTabPosition = $(this).position().left;
+		$('.tab-bg').stop().animate({'left': $currentTabPosition}, 200);
+	});
+
+	$('.tabs > div').mouseleave(function() {
+		var $activeTabPosition = $('.active').position().left;
+		$('.tab-bg').stop().animate({'left': $activeTabPosition}, 200);
+	});
+
+});
