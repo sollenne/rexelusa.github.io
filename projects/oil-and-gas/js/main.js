4ce01e0e5fa3bcc8715e3cb44327568055007a03
Rich Sollenne <Rich.Sollenne@Rexel.com>
2015-03-30 13:25:47 -0500

    oil and gas for demo

--- projects/oil-and-gas/js/main.js
+++ projects/oil-and-gas/js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 	$('.tabs > div').click(function() {
 		var $currentTabIndex = $(this).index();
+		var $innerHeight = $('.content-inner').find('section').eq($currentTabIndex).height();
+		console.log($innerHeight);
+		$('.content-inner').stop().animate({'height': $innerHeight + 50}, 200);
 		$('.content-inner').find('section').eq($currentTabIndex).stop().fadeIn().siblings().fadeOut();
 		$(this).addClass('active').siblings().removeClass('active');
+
 
 		var $currentTabPosition = $(this).position().left;
 		$('.tab-bg').stop().animate({'left': $currentTabPosition});
@@ -17,5 +21,7 @@ $(document).ready(function() {
 		var $activeTabPosition = $('.active').position().left;
 		$('.tab-bg').stop().animate({'left': $activeTabPosition}, 200);
 	});
+
+
 
 });
